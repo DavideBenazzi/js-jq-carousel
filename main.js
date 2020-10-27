18babf4d6660c66d779eb63e0027c6140f9d475b
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-27 15:29:05 +0100

    Funzione Next

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 $(document).ready( function() {
-  
+  var prev = $('body .container .slider-wrapper .prev');
+  var next = $('body .container .slider-wrapper .next');
+
+  //NAVIGA PRECEDENTE
+  $(prev).click( function() {
+    prevNext('prev');
+  })
+  //NAVIGA SUCCESSIVO
+  $(next).click( function() {
+    prevNext('next');
+  })
 })
+
+//FUNZIONE NAVIGA AVANTI INDIETRO
+function prevNext(direction) {
+
+  var imgActive = $('body .container .slider-wrapper .images img.active');
+  var navActive = $('body .container .slider-wrapper .nav i.active');
+
+  //RESET ACTIVE
+  imgActive.removeClass('active');
+  navActive.removeClass('active');
+
+  //DIREZIONE NEXT
+  if (direction === 'next') {
+    $(imgActive).next('img').addClass('active');
+    $(navActive).next('i').addClass('active');
+  }
+
+}
